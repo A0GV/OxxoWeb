a0fddc2c58571b77848639df42d3254b8791b1dd
Monica Guzman <a00838824@tec.mx>
2025-03-12 15:12:46 -0600

    Basic database reading

--- OxxoWeb/Models/LeaderJ1.cs
+++ OxxoWeb/Models/LeaderJ1.cs
@@ -5,6 +5,7 @@ using System;
 // Conectar a modelo oxxo web
 namespace OxxoWeb.Models
 {
+    /*
     public class LeaderJ1
     {
         // J1 valores
@@ -54,4 +55,33 @@ namespace OxxoWeb.Models
         // Constructor sin parametros
         public LeaderJ1() {}
     }
+    */
+
+    public class LeaderJ1
+    {
+        // Usuario
+        public int id_usuario { get; set; }
+        public int id_plaza { get; set; }
+        public string nombre { get; set; }
+        public string apellido_pat { get; set; }
+        public string apellido_mat { get; set; }
+
+        // Total EXP (Aggregated from SUM(j1.exp))
+        public int total_exp { get; set; }
+
+        // Constructor with parameters
+        public LeaderJ1(int id_usuario_, int id_plaza_, string nombre_, string apellido_pat_, string apellido_mat_, int total_exp_)
+        {
+            this.id_usuario = id_usuario_;
+            this.id_plaza = id_plaza_;
+            this.nombre = nombre_;
+            this.apellido_pat = apellido_pat_;
+            this.apellido_mat = apellido_mat_;
+            this.total_exp = total_exp_;
+        }
+
+        // Default Constructor
+        public LeaderJ1() { }
+    }
 }
+
