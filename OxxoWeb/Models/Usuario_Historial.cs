a7a700ce1ca6f2bfb73c6767e44786068cb2fcb6
Monica Guzman <a00838824@tec.mx>
2025-03-12 10:56:23 -0600

    Added Usuario, Usuario_Historial, and Historial entity classes

---
+++ OxxoWeb/Models/Usuario_Historial.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+// Conectar a modelo oxxo web
+namespace OxxoWeb.Models
+{
+    public class Usuario_Historial
+    {
+        // Valores de entidad
+       public int id_historial {get;set;}
+        public int id_usuario {get;set;}
+
+        // Con parámetros
+        public Usuario_Historial(int id_historial_,int id_usuario_)
+        {
+            this.id_historial = id_historial_;
+            this.id_usuario = id_usuario_;
+        }
+
+        // Constructor sin parámetros
+        public Usuario_Historial() {}
+    }
+}
