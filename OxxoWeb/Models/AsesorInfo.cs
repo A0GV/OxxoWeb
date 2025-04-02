6b3128b7adf990aef39ec7a8d407d829d4a07050
Regina <a00840840@tec.mx>
2025-04-02 09:29:24 -0600

    update gerente

---
+++ OxxoWeb/Models/AsesorInfo.cs
@@ -0,0 +1,15 @@
+namespace OxxoWeb.Models;
+
+public class AsesorInfo
+{
+    public int IdUsuario { get; set; }
+    public string Nombre { get; set; }
+    public string ApellidoPat { get; set; }
+    public string ApellidoMat { get; set; }
+    public string Correo { get; set; }
+    public string PlazaNombre { get; set; }
+    public string Ciudad { get; set; }
+    public string Estado { get; set; }
+    public string TipoUsuario { get; set; }
+    public string Foto { get; set; }
+}
