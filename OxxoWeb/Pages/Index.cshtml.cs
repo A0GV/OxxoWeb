fe4b7abe7c95c69bf9bc6081133867855f9c77bc
Alejandro Uribe <alexuribemoreno@gmail.com>
2025-04-02 00:37:36 -0600

    Update Index.cshtml.cs
    Para poder abrir la pagina del usuario

--- OxxoWeb/Pages/Index.cshtml.cs
+++ OxxoWeb/Pages/Index.cshtml.cs
@@ -25,17 +25,32 @@ namespace OxxoWeb.Pages
 
             if (isValid)
             {
-                int UserSessionId = context.GetUserId(Username);
-                if (UserSessionId !=0)
+                int userSessionId = GetUserId(Username); 
+                if (userSessionId != 0)
                 {
-                    HttpContext.Session.SetInt32("Id", UserSessionId);
-                };
+                    HttpContext.Session.SetInt32("Id", userSessionId); 
+                }
                 return RedirectToPage("/Home");
             }
             else
             {
-                ErrorMessage = "El usuario o contraseña son incorrectos"; // Establece el mensaje de error
+                ErrorMessage = "El usuario o contraseña son incorrectos";
                 return Page();
+            }
+        }
+
+        private int GetUserId(string username)
+        {
+            using (var connection = new MySql.Data.MySqlClient.MySqlConnection("Server=127.0.0.1;Port=3306;Database=oxxo_base_e2_1;Uid=root;password=root"))
+            {
+                connection.Open();
+                string query = "SELECT id_usuario FROM usuario WHERE correo = @username LIMIT 1;";
+                using (var command = new MySql.Data.MySqlClient.MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    var result = command.ExecuteScalar();
+                    return result != null ? Convert.ToInt32(result) : 0;
+                }
             }
         }
     }
