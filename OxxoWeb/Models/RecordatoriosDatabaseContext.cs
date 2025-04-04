575642345821301ecca869a7bc99008dac4f942b
Cielo Maria Vega Godoy <a01741542@tec.mx>
2025-04-04 01:39:05 -0600

    Add Recordatorios feature with database context and category retrieval

--- OxxoWeb/Models/RecordatoriosDatabaseContext.cs
+++ OxxoWeb/Models/RecordatoriosDatabaseContext.cs
@@ -26,5 +26,33 @@ namespace OxxoWeb.Models {
         {
             return new MySqlConnection(ConnectionString);
         }
+
+        public List<Categoria> GetCategorias(int idUSuario){
+            List<Categoria> categorias = new List<Categoria>();
+            MySqlConnection conexion = GetConnection();
+            conexion.Open();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conexion;
+            cmd.Parameters.AddWithValue("@id", idUSuario);
+            cmd.CommandText = @"select ut.id_tienda, t.nombre_calle from usuario u
+                            join usuario_tienda ut on ut.id_usuario = u.id_usuario
+                            join tienda t on t.id_tienda = ut.id_tienda
+                            where u.id_usuario = @id;";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Categoria cat = new Categoria(
+                        reader.GetInt32("id_tienda"),
+                        reader.GetString("nombre_calle")
+                    );
+                    categorias.Add(cat);
+                }
+            }                
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+            return categorias;
+        }
     }
 }
--- OxxoWeb/Pages/Recordatorios.cshtml.cs
+++ OxxoWeb/Pages/Recordatorios.cshtml.cs
@@ -1,10 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using OxxoWeb.Models; // Busca en carpeta de Models
 
 namespace OxxoWeb.Pages;
 
 public class RecordatoriosModel : PageModel
 {
+    private readonly RecordatoriosDataBaseContext context;
+    public List<Categoria> categorias {get;set;}
+    public Dictionary<int,string> colores = new Dictionary<int, string>{
+        {1,"morado"},{2,"amarillo"},{3,"rosa"},{0,"verde"}
+    };
 
+    public RecordatoriosModel(RecordatoriosDataBaseContext _context){
+        context = _context;
+        //Inicializar
+        categorias = new List<Categoria>();
+    }
+    public void OnGet(){
+        // Checks if it has a user ID stored as int
+        int? userId = HttpContext.Session.GetInt32("Id");
+        if (userId == null)
+        {
+            // Takes user to login page
+            Response.Redirect("/Index");
+        } else{
+            //Obtiene las categorías de tienda
+            categorias = context.GetCategorias(userId.Value);
+            categorias.ForEach(categoria => categoria.color = colores[categoria.id_tienda % 4]);
+        }
+    }
 }
