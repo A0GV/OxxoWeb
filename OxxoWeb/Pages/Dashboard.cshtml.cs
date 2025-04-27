7709c08e8f43f5549f123321a4aedaf353149e4e
Monica Guzman <a00838824@tec.mx>
2025-04-26 18:01:06 -0600

    Ya esta iframe de usuario fijo 7

---
+++ OxxoWeb/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OxxoWeb.Models;
+
+namespace OxxoWeb.Pages;
+
+public class DashboardModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
