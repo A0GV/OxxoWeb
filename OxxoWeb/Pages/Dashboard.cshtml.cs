e1b1a141691b17c7e688813c87bfaddd77338f29
Monica Guzman <a00838824@tec.mx>
2025-04-26 18:40:36 -0600

    Consigue perfil de usuario seleccionado

--- OxxoWeb/Pages/Dashboard.cshtml.cs
+++ OxxoWeb/Pages/Dashboard.cshtml.cs
@@ -7,7 +7,31 @@ namespace OxxoWeb.Pages;
 
 public class DashboardModel : PageModel
 {
+    private readonly DataBaseContextPerfil _context; // Para poder usar ese database context para el tipo de usuario
+    public Perfiles UsuarioPerfil { get; set; } // Obtener el tipo de sesión del usuario
+
+    // Incluir el model en el servicio 
+    public DashboardModel(DataBaseContextPerfil context)
+    {
+        _context = context;
+    }
     public void OnGet()
     {
+        
+        // Checks if it has a user ID stored as int
+        int? userId = HttpContext.Session.GetInt32("Id");
+        if (userId == null)
+        {
+            // Takes user to login page
+            Response.Redirect("/Index");
+        }
+        else 
+        {
+            // Cargar el perfil del usuario especificado
+            UsuarioPerfil = _context.GetPerfil(userId.Value);
+
+            
+
+        }
     }
 }
